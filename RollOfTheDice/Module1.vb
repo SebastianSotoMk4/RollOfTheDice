20b630782d6521bbeeb196120d98becb396c19a4
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-28 08:54:26 -0600

    Add project files.

---
+++ RollOfTheDice/Module1.vb
@@ -0,0 +1,14 @@
+﻿'Sebastian Soto
+'RCET0265
+'Fall 2021
+'_projectName
+'_gitURL
+Option Strict On
+Option Explicit On
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ RollOfTheDice/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RollOfTheDice")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("RollOfTheDice")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2300972d-3225-4796-9f91-cfb8f79e988e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
