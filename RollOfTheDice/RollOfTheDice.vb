228ae4663bc5bf7c6ae3589f860ebcadd8ce5133
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-28 08:56:39 -0600

    title block and Links

---
+++ RollOfTheDice/RollOfTheDice.vb
@@ -0,0 +1,15 @@
+﻿'Sebastian Soto
+'RCET0265
+'Fall 2021
+'Roll Of The Dice
+'https://github.com/SebastianSotoMk4/RollOfTheDice.git
+Option Strict On
+Option Explicit On
+Module RollOfTheDice
+
+    Sub Main()
+
+    End Sub
+
+End Module
+'https://elearn.isu.edu/moodle/mod/assign/view.php?id=2180759
